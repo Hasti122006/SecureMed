90ccde15a10b7d0ec7cf2d65f7c0a2131c1fc927
Hasti Momaya <hastimomaya012@gmail.com>
2026-07-08 12:59:29 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/SecureMed/",
+
   server: {
     host: "localhost",
     port: 5173,
@@ -13,7 +14,12 @@ export default defineConfig(({ mode }) => ({
       overlay: true,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
